c554b219c25297c4f329a9cac427a2b8811e487d
rogeriodelphi <rogeriodelphi@gmail.com>
2023-01-09 02:43:12 -0300

    Refactoring the code

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    body: Column(
-      children: [
-        Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100,00'),
-              subtitle: Text('1.000,00'),
-            )
-        ),
-        Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('200,00'),
-              subtitle: Text('1.000,00'),
-            )
-        ),
-        Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100,00'),
-              subtitle: Text('1.000,00'),
-            )
-        ),
-      ],
-    ),
+    body: ListaTransferencias(),
     appBar: AppBar(title: Text('Transferências'),),
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
@@ -34,4 +10,48 @@ void main() => runApp(MaterialApp(
     ),
   ),
 ));
+
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(1000.11, 27721)),
+        ItemTransferencia(Transferencia(2000.22, 27722)),
+        ItemTransferencia(Transferencia(3000.33, 27723)),
+      ],
+    );
+  }
+}
+
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement Build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      )
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
+
+
 
